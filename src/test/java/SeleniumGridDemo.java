1b13b6142881e4dac9a5b98beec76dbb21c0497b
Kundan Sharma <universalitservices005@gmail.com>
2023-07-15 07:17:30 +0100

    Updated for webdrivermanager

--- src/test/java/SeleniumGridDemo.java
+++ src/test/java/SeleniumGridDemo.java
@@ -1,13 +1,12 @@
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
